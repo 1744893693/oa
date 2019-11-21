bd2fa5bbdd072bbcbb4540a783edc38159f02453
DY <1744893693@qq.com>
2019-11-21 08:33:50 +0800

    aa

--- app/admin/controller/Status.php
+++ app/admin/controller/Status.php
@@ -17,10 +17,11 @@ class  status
 
         include_once './app/admin/view/status/company.php';
     }
+
     function up(){
-   $d= new Model();
-   $id=$_POST['id'];
-   $d->sql_operation("update company set status=1 WHERE id='$id'");
+        $d= new Model();
+        $id=$_POST['id'];
+        $d->sql_operation("update company set status=1 WHERE id='$id'");
 
     }
 
@@ -42,13 +43,20 @@ class  status
         $a=$_POST['company_name'];
         $b=$_POST['legal_person'];
         $id=$_POST['id'];
-       $date= $d->sql_operation("update company set company_name='$a',legal_person='$b' WHERE  id='$id'");
+        $date= $d->sql_operation("update company set company_name='$a',legal_person='$b' WHERE  id='$id'");
         return $date;
     }
 
     function layuia(){
         $d=new Model();
-        $data=$d->selects();
+        if(!empty($_POST['send_name'])){
+            $data=$d->sql_operation("select * from company where company_name like '%$_POST[send_name]%' or legal_person like '%$_POST[send_name]%' or id like '%$_POST[send_name]%'");
+        }else{
+            $data=$d->selects();
+        }
+        if(empty($data)){
+            $data=$d->selects();
+        }
         if($data){
             $d=[];
             $d['code']=0;
@@ -56,7 +64,7 @@ class  status
             $d['msg']="";
             $d['data']=$data;
         }
-       echo json_encode($d);
+        echo json_encode($d);
 
     }
 }
