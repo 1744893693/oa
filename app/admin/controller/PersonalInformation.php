59c7b708e93e92cbd3515fd819363f7764b633ff
Your Name <you@example.com>
2019-11-21 19:57:02 +0800

    aa

---
+++ app/admin/controller/PersonalInformation.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/11/21
+ * Time: 13:52
+ */
+
+namespace app\admin\controller;
+
+
+class PersonalInformation
+{
+    function init(){
+        echo $_GET['id'];
+    }
+}
