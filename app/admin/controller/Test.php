2eaaeeec3ae6ffd90f8f5e293ca74ead880f0c08
DY <1744893693@qq.com>
2019-11-26 17:08:12 +0800

    aa

--- app/admin/controller/Test.php
+++ app/admin/controller/Test.php
@@ -36,6 +36,7 @@ class Test extends Login{
         $submission_time = $_POST['submission_time'];
         $test_content= $_POST['test_content'];
         $department_id= $_POST['department_id'];
+        $company_id=$_SESSION['admin']['company_id'];
         $b = $aa->sql_operation("select name from department where id='$department_id' ");
         if(empty($test_name)||empty($release_time)){
             exit(json_encode(array('v'=>201,'data'=>'时间不能为空！')));
@@ -43,8 +44,12 @@ class Test extends Login{
         if(empty($test_content)){
             exit(json_encode(array('v'=>202,'data'=>'请详述任务内容！')));
         }
+        if(empty($department_id)){
+            exit(json_encode(array('v'=>203,'data'=>'请指定任务部门！')));
+        }
         $p=$b[0]['name'];
-        $v = $aa->sql_operation("insert into test (name,test_name,release_time,submission_time,test_content,department_id) VALUES  ('$name','$test_name','$release_time','$submission_time','$test_content','$p')");
+        $v = $aa->sql_operation("insert into test (name,test_name,release_time,submission_time,test_content,department_id,company_id) 
+                                 VALUES  ('$name','$test_name','$release_time','$submission_time','$test_content','$p','$company_id')");
 
         if($v){
             exit(json_encode(array('v'=>1,'data'=>'请等待领取！')));
