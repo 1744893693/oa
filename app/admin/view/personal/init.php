9f9bd92a4a279f47e4be4cd5aba5802d463b9ccf
Your Name <you@example.com>
2019-11-28 15:39:27 +0800

    aa

--- app/admin/view/personal/init.php
+++ app/admin/view/personal/init.php
@@ -26,7 +26,6 @@
 
 <script type="text/html" id="barDemo">
     <a class="layui-btn layui-btn-xs" lay-event="edit">修改</a>
-    <a class="layui-btn layui-btn-danger layui-btn-xs" lay-event="del">删除</a>
 </script>
 <div id="tan" style="display: none;margin-top:30px">
     <div style=" text-align:center" class="layui-form-item" id="id">
